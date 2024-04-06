f65fa2933b66b796013d0038b0e78c7d088bda6c
Katherine Meadows <kmeadows3@gmail.com>
2024-04-06 10:26:42 -0400

    Restructured database

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,10 +3,14 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO tag (tag) VALUES ('help');
+INSERT INTO response(response, name) VALUES ('This is a response to a help request.', 'chatbot_support');
 
-INSERT INTO response(response) VALUES ('This is a response to a help request.');
+INSERT INTO intent(name) VALUES ('Support');
 
-INSERT INTO response_tag(response_id, tag_id) VALUES (1,1);
+INSERT INTO entity(name) VALUES ('Chatbot');
+
+INSERT INTO keyword(keyword, intent_id, entity_id) VALUES ('help', 1, NULL);
+INSERT INTO keyword(keyword, intent_id, entity_id) VALUES ('chatbot', NULL, 1);
+
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, response_tag, response, tag;
+DROP TABLE IF EXISTS users, response_intent, response_entity, keyword, entity, intent, response;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -12,20 +12,44 @@ CREATE TABLE users (
 
 CREATE TABLE response(
 	response_id serial PRIMARY KEY,
-	response text NOT NULL
+	response text NOT NULL,
+	name varchar(50)
 );
 
-CREATE TABLE tag(
-	tag_id serial PRIMARY KEY,
-	tag varchar(50) NOT NULL
+CREATE TABLE intent(
+	intent_id serial PRIMARY KEY,
+	name varchar(50) NOT NULL
 );
 
-CREATE TABLE response_tag(
-	tag_id int NOT NULL,
+CREATE TABLE response_intent(
+	intent_id int NOT NULL,
 	response_id int NOT NULL,
-	CONSTRAINT PK_response_tag PRIMARY KEY (tag_id, response_id),
-	CONSTRAINT FK_response_tag_tag_id FOREIGN KEY(tag_id) REFERENCES tag(tag_id),
-	CONSTRAINT FK_response_tag_response_id FOREIGN KEY(response_id) REFERENCES response(response_id)
+	CONSTRAINT PK_response_intent PRIMARY KEY (intent_id, response_id),
+	CONSTRAINT FK_intent_id_intent_id FOREIGN KEY(intent_id) REFERENCES intent(intent_id),
+	CONSTRAINT FK_response_id_response_id FOREIGN KEY(response_id) REFERENCES response(response_id)
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE entity(
+	entity_id serial PRIMARY KEY,
+	name varchar(50) NOT NULL
+);
+
+CREATE TABLE response_entity(
+	response_id int NOT NULL,
+	entity_id int NOT NULL,
+	CONSTRAINT PK_response_entity PRIMARY KEY (entity_id, response_id),
+	CONSTRAINT FK_entity_id_entity_id FOREIGN KEY(entity_id) REFERENCES entity(entity_id),
+	CONSTRAINT FK_response_id_response_id FOREIGN KEY(response_id) REFERENCES response(response_id)
+);
+
+CREATE TABLE keyword(
+	keyword_id serial PRIMARY KEY,
+	keyword varchar(100) NOT NULL,
+	entity_id int,
+	intent_id int,
+	CONSTRAINT FK_keyword_entity_id FOREIGN KEY(entity_id) REFERENCES entity(entity_id),
+	CONSTRAINT FK_keyword_intent_id FOREIGN KEY(intent_id) REFERENCES intent(intent_id)
+);
+
+
+COMMIT TRANSACTION; 
