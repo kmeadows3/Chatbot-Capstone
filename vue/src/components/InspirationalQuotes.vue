9698b7c43de935fba6f6b4e2671198fc733bd891
Cameron Coe <bransfordc@gmail.com>
2024-04-10 11:22:00 -0400

    Making sure the database is sent

---
+++ vue/src/components/InspirationalQuotes.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Inspirational Quote of the Minute:</h1>
+    <h2>Quote will go here</h2>
+    <p></p>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,15 +2,18 @@
   <div class="home">
     <h1>Home</h1>
     <ChatDisplay/>
+    <InspirationalQuotes />
   </div>
 </template>
 
 <script>
 import ChatDisplay from '../components/ChatDisplay.vue';
+import InspirationalQuotes from '../components/InspirationalQuotes.vue'
 
 export default {
   components: {
-    ChatDisplay
+    ChatDisplay,
+    InspirationalQuotes
   }
 };
 </script>
