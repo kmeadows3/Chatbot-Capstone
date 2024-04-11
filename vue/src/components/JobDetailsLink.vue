139859860f615005a95cad5a58cedd6e2aef0cd9
Cameron Coe <bransfordc@gmail.com>
2024-04-11 16:56:42 -0400

    Created Job Posting Components

---
+++ vue/src/components/JobDetailsLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <a href="#" v-on:submit.prevent="() => selectJob()">{{ this.text }}</a>
+</template>
+
+<script>
+export default {
+    params: {
+        jobPosting: {},
+        text: "",
+    },
+
+    methods: {
+        selectJob() {
+            this.$store.commit("SET_SELECTED_JOB_POSTING", this.jobPosting);
+        }
+    }
+}
+</script>
+
+<style></style>
