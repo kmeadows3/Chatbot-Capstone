7b1e8e80b023295c6651c844c7cd888485cf2ef9
Cameron Coe <bransfordc@gmail.com>
2024-04-11 08:54:44 -0400

    Integrated the inspirational quote image API

--- vue/src/services/imageService.js
+++ vue/src/services/imageService.js
@@ -1,17 +1,12 @@
-import axios from 'axios';
+import { createClient } from 'pexels'; 
 
-// API Documentation:  https://picsum.photos/
-
-const imageHttp = axios.create({
-    baseURL: 'https://picsum.photos'
-});
+// Initialize the Pexels client with your API key
+const client = createClient('vBMRgCXuDHQ3BU05Pm2EghNw4Hu1HoeN9q2ra2ua3Jy1S6sGh3TUrnxe');
 
 export default {
-    get(width, height) {
-        let apiCommand = '/';
-        apiCommand += width;
-        apiCommand += '/' + height;
-
-        imageHttp.get('');
+    getRandom(pageNumber, photosPerPage) {
+        // Make a request to the Pexels API to search for photos with the query 'Nature' and limit to 1 result
+        return client.photos.search({ query: 'Nature', page: pageNumber, per_page: photosPerPage });
+            
     }
 }
