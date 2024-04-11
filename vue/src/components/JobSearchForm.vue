c482fff4d5af22b2bc91299272f527d391567a32
Cameron Coe <bransfordc@gmail.com>
2024-04-11 15:52:50 -0400

    Updates to the job posting service

--- vue/src/components/JobSearchForm.vue
+++ vue/src/components/JobSearchForm.vue
@@ -1,22 +1,183 @@
 <template>
     <H1>Job Search Form</H1>
-    <form id="job_search_form"  @click.prevent="() => handleClick(5)">
+    <form id="job_search_form"  v-on:submit.prevent="() => searchJobs()">
+        <label for="city">City Name: </label>
+        <input id="city" type="text" v-model="city">
+        <br/>
+        <label for="stateAbbreviation">State Abbreviation: </label>
+        <input id="stateAbbreviation" type="text" v-model="stateAbbreviation">
+        <br/>
+        <label for="includeRemote">Include Remote Positions: </label>
+        <input id="includeRemote" type="checkbox" v-model="includeRemote">
+        <br/>
         <input type="submit" value="Search Job">
     </form>
 </template>
 
 <script>
-export default {
+import JobSearchService from '../services/JobSearchService';
 
+export default {
+    
+    data() {
+        return {
+            city: '',
+            stateAbbreviation: '',
+            includeRemote: false,
+            jobPostings: [],
+        }
+    },
 
     methods: {
-        handleClick(x) {
-            console.log('Button clicked!' + x);
-        },
         searchJobs() {
-            console.log("Job Search Works");
+            this.jobPostings = [];
+            this.searchJobsByPage(1);
+            console.log(this.jobPostings);
+            
+        },
+
+        searchJobsByPage(pageNumber) {
+            JobSearchService.get(this.urlFormattedCityName, this.formattedStateAbbreviation, pageNumber)
+            .then(response => {
+                const jobPostings = response.data;
+                const remote = 'Flexible / Remote';
+
+                // Add jobs matching the input location
+                const filteredJobPostings = this.filterJobPostingsByLocation(jobPostings, this.formattedLocation);
+
+                // Add remote positions if selected
+                if (this.includeRemote) {
+                    const remoteJobPostings = this.filterJobPostingsByLocation(jobPostings, remote);
+                }
+
+                
+            })
+            .catch(error => {
+                console.error("Error in Job Search " + error);
+            });
+        },
+
+        // Returns an array of job postings with a matching location
+        filterJobPostingsByLocation(jobPostings, searchLocation) {
+            const results = jobPostings.results;
+            const filteredJobPostings = results.filter((result) => {
+                let hasMatchingLocation = false;
+
+                const locations = result.locations;
+                locations.forEach((currentLocation) => {
+                    if(currentLocation.name === searchLocation) {
+                        hasMatchingLocation = true;
+                    }
+                });
+
+                return hasMatchingLocation;
+            });
+
+            return filteredJobPostings;
+        },
+
+        addJobPostingsToListInStore(jobPostings) {
+            jobPostings.forEach(currentJobPosting => {
+                const jobPostingToAdd = {
+                    positionTitle: currentJobPosting.name,
+                    companyName: currentJobPosting.company.name,
+                    companyId: currentJobPosting.company.id,
+                    locations: currentJobPosting.locations,
+                    description: currentJobPosting.contents,
+                    levels: currentJobPosting.levels,
+                    landingPage: currentJobPosting.refs.landing_page,
+                }
+
+                this.addJobPosting(jobPostingToAdd);
+            })
+        },
+
+        addJobPosting(jobPosting) {
+            this.$store.commit("ADD_JOB_POSTING", jobPosting);
         }
+    },
+
+    computed: {
+        formattedCityName(){
+            let cityName = this.city;
+
+            // Get all words in city name
+            let words = cityName.split(" ");
+
+            // remove all empty strings
+            words = words.filter((word) => {
+                return word.length > 0;
+            });
+
+            // Capitalize each word
+            words = words.map(word => {
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            });
+
+            // Join and separate words with a space
+            cityName = words.join(" ");
+
+            if (cityName !== undefined) {
+                return cityName;
+            } else {
+                return "";
+            }
+        },
+
+        formattedStateAbbreviation() {
+            let formattedStateAbbreviation = this.stateAbbreviation.toUpperCase();
+            
+            // Remove Spaces
+            let words = formattedStateAbbreviation.split(" ");
+
+            words = words.filter((word) => {
+                return word.length > 0;
+            });
+
+            formattedStateAbbreviation = words.join("");
+
+
+            if (formattedStateAbbreviation !== undefined) {
+                return formattedStateAbbreviation;
+            } else {
+                return "";
+            }
+        },
+
+        formattedLocation() {
+            const city = this.formattedCityName;
+            const state = this.formattedStateAbbreviation;
+            return  city + ", " + state;
+        },
+
+        urlFormattedCityName(){
+            let cityName = this.city;
+
+            // Get all words in city name
+            let words = cityName.split(" ");
+
+            // remove all empty strings
+            words = words.filter((word) => {
+                return word.length > 0;
+            });
+
+            // Capitalize each word
+            words = words.map(word => {
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            });
+
+            // Join and separate words with a url space
+            cityName = words.join("%20");
+
+            if (cityName !== undefined) {
+                return cityName;
+            } else {
+                return "";
+            }
+        },
+
     }
+
 }
 </script>
 
