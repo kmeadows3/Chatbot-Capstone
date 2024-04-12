b173a989aedb0557ff1042f8301a45235bcda635
Cameron Coe <bransfordc@gmail.com>
2024-04-12 09:06:51 -0400

    pulling

--- vue/src/components/JobDetails.vue
+++ vue/src/components/JobDetails.vue
@@ -1,13 +1,15 @@
 <template>
-    <h1>Current Job Posting</h1>
-    <p>{{ this.$store.state.selectedJobPosting.positionTitle }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.levels }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.companyName }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.companyId }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.locations }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.description }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.datePosted }}</p>
-    <p>{{ this.$store.state.selectedJobPosting.landingPage }}</p>
+    <div class ="job_details">
+        <h1>Current Job Posting</h1>
+        <p>Position Title: {{ this.$store.state.selectedJobPosting.positionTitle }}</p>
+        <p>Position Level: {{ this.$store.state.selectedJobPosting.levels }}</p>
+        <p>Company Name: {{ this.$store.state.selectedJobPosting.companyName }}</p>
+        <p>Company ID: {{ this.$store.state.selectedJobPosting.companyId }}</p>
+        <p>Locations: {{ this.$store.state.selectedJobPosting.locations }}</p>
+        <p>Description: {{ this.$store.state.selectedJobPosting.description }}</p>
+        <p>Date Posted: {{ this.$store.state.selectedJobPosting.datePosted }}</p>
+        <p>Landing Page: {{ this.$store.state.selectedJobPosting.landingPage }}</p>
+    </div>
 </template>
 
 <script>
@@ -16,4 +18,14 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+div.job_details {
+    background-color: rgb(234, 234, 234);
+    border: 2px solid rgb(67, 67, 67);
+    border-radius: 8px;
+    width: 500px;
+    padding: 10px;
+    
+}
+
+</style>
