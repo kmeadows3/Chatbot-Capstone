a670137c764fc533aa5d8f4b76950b4f3d04b1be
Cameron Coe <bransfordc@gmail.com>
2024-04-13 12:58:14 -0400

    Updates to display

--- vue/src/components/JobDetails.vue
+++ vue/src/components/JobDetails.vue
@@ -1,18 +1,16 @@
 <template>
-    <div class ="job_details">
-        <div class="job_search_results" v-show="$store.state.jobPostings.length > 0">
-            <h1>Recent Job Posting{{ $store.state.jobPostings.length > 1 ? 's': '' }}:</h1>
-            <div v-for="jobPosting in $store.state.jobPostings">
-                <div class="job_card" target="_blank">
-                    <p>Position Title: {{ jobPosting.positionTitle }}</p>
-                    <p>Position Level: {{ jobPosting.levels }}</p>
-                    <p>Company Name: {{ jobPosting.companyName }}</p>
-                    <p>Company ID: {{ jobPosting.companyId }}</p>
-                    <p>Date Posted: {{ jobPosting.datePosted }}</p>
-                    <p>Locations: {{ jobPosting.locations }}</p>
-                    <p class="description">Description: <span v-html="jobPosting.description"></span></p>
-                    <p>Landing Page: <a v-bind:href="jobPosting.landingPage" target="_blank">{{ jobPosting.landingPage }}</a></p>
-                </div>
+    <div class ="job_details" v-show="$store.state.jobPostings.length > 0">
+        <h1>Recent Job Posting{{ $store.state.jobPostings.length > 1 ? 's': '' }}:</h1>
+        <div v-for="jobPosting in $store.state.jobPostings">
+            <div class="job_card" target="_blank">
+                <p>Position Title: {{ jobPosting.positionTitle }}</p>
+                <p>Position Level: {{ jobPosting.levels }}</p>
+                <p>Company Name: {{ jobPosting.companyName }}</p>
+                <p>Company ID: {{ jobPosting.companyId }}</p>
+                <p>Date Posted: {{ jobPosting.datePosted }}</p>
+                <p>Locations: {{ jobPosting.locations }}</p>
+                <p class="description">Description: <span v-html="jobPosting.description"></span></p>
+                <p>Landing Page: <a v-bind:href="jobPosting.landingPage" target="_blank">{{ jobPosting.landingPage }}</a></p>
             </div>
         </div>
     </div>
@@ -26,35 +24,36 @@ export default {
 
 <style>
 div.job_details {
-    background-color: rgb(234, 234, 234);
-    border: 2px solid rgb(67, 67, 67);
-    border-radius: 8px;
-    width: 100%;
-    height: 100%;
-    overflow-y: auto;
-    overflow: hidden; /* Makes a window for all the elements inside */
-    position: relative;
+    max-width: 100%;
+    max-height: 100%;
+    overflow-y: auto; /* Enable vertical scrolling */
+    display: flex; /* Use flexbox to control layout */
+    flex-direction: column; /* Arrange child elements vertically */
+    align-items: center;
 }
 
-div.job_postings::-webkit-scrollbar {
+div.job_search_results::-webkit-scrollbar {
     width: 10px;
 }
 
-div.job_postings::-webkit-scrollbar-thumb {
+div.job_search_results::-webkit-scrollbar-thumb {
     background-color: #8888;
     border-radius: 10px;
 }
 
-div.job_postings::-webkit-scrollbar-track {
+div.job_search_results::-webkit-scrollbar-track {
     background-color: #f5f5f5;
 }
+
+
 
 div.job_card {
     background-color: rgb(21, 255, 185);
     border-radius: 8px;
-    width:90%;
+    width: 90%;
     padding: 10px;
-    margin: 10px;
+    margin-left: 3%;
+    margin-bottom: 20px;
 }
 
 
