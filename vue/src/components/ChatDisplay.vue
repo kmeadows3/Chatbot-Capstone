ce3c8e08c517e39b2b42de986c0055782ab1457f
Cameron Coe <bransfordc@gmail.com>
2024-04-15 11:41:52 -0400

    updated job description cards

--- vue/src/components/ChatDisplay.vue
+++ vue/src/components/ChatDisplay.vue
@@ -221,14 +221,21 @@ export default {
             name = this.removeFromName(name, ".");
             name = this.removeFromName(name, ". ");
             name = this.removeFromName(name, "?");
+            name = this.removeFromName(name, "<");
+            name = this.removeFromName(name, ">");
+            name = this.removeFromName(name, "/");
+            name = this.removeFromName(name, `"`);
+            name = this.removeFromName(name, "`");
+            name = this.removeFromName(name, `'`);
             this.$store.commit('SET_PREFERREDNAME', name);
             greetUser = true;
         },
 
         removeFromName(oldString, textToReplace) {
+            const escapedText = textToReplace.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            const regex = new RegExp(escapedText.toUpperCase(), 'g');
             oldString = oldString.toUpperCase();
-            textToReplace = textToReplace.toUpperCase();
-            return oldString.replace(textToReplace, "");
+            return oldString.replace(regex, "");
         },
 
         greetUser() {
