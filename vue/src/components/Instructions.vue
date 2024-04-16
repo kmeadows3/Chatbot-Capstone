d99c52d0c7953848a145a0a81283ae90b13c0e4a
Katherine Meadows <kmeadows3@gmail.com>
2024-04-16 10:37:24 -0400

    disabled textarea during quiz

--- vue/src/components/Instructions.vue
+++ vue/src/components/Instructions.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="container">
+        <h1>CHATWICK</h1>
+        <div>
+            <p class='welcome'>Welcome to Chatwick, the Tech Elevator job search and interview prep chatbot.</p>
+        </div>
         <div class='examples'>
             <p>Some example command you can give Chatwick are:</p>
             <ul>
@@ -7,11 +11,12 @@
                 <li>“What is the STAR Method?”</li>
                 <li>“I want to search for jobs.”</li>
                 <li>“I would like company information.”</li>
+                <li>“Give me a technical quiz.”</li>
                 <li>“Chatbot support.”</li>
             </ul>
         </div>
         <div class = 'Credits'>
-            <p> Coded by: Cameron Coe, Eric Kuklinski, Hassan Mohamud, and Katherine Meadows</p>
+            <p class='credits'> Coded by: Cameron Coe, Eric Kuklinski, Hassan Mohamud, and Katherine Meadows</p>
         </div>    
     </div>
 </template>
@@ -23,16 +28,34 @@ div.container {
     display: flex;
     flex-direction: column;
     justify-content:space-between;
-    height: auto;
-    margin: 20px;
+    height: 90%;
+    margin: 20px auto 10px auto;
+    width: 90%;
+    border: solid 1px black;
 }
 
 h1 {
-    margin: 0%;
+    margin: 0px;
+    padding: 0px;
+    text-align: center;
+    font-family: Luminari;
 }
 
 div.credits {
     margin: 0%;
 }
 
+p.welcome {
+    text-align: center;
+}
+
+div.examples{
+    margin: 10px 30px;
+    align-self: center;
+}
+
+p.credits {
+    text-align: center;
+    font-size: .8em;
+}
 </style>
