8d957a01a8d0b26d35273ef3545ca0a7ba2d543f
Katherine Meadows <kmeadows3@gmail.com>
2024-04-16 17:23:24 -0400

    Added keywords, gave feedback when user answers question

--- vue/src/components/ChatDisplay.vue
+++ vue/src/components/ChatDisplay.vue
@@ -413,7 +413,6 @@ div#chat-display>div {
     flex-direction: column;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
     height: auto;
-
 }
 
 div#user-input {
@@ -433,13 +432,19 @@ img.response_img {
 div.chatbot {
     align-self: start;
     background-color: #e1ffed;
+    overflow-wrap: break-word;
+
 }
 
+div {
+    overflow-wrap: break-word;
+}
 
 div.user {
     align-self: end;
     background-color: #e7e0ff;
     align-items: flex-end;
+    overflow-wrap: break-word;
 }
 
 .user-text-div {
@@ -468,6 +473,8 @@ div.user {
     display: flex;
     align-items: center;
     gap: 8px;
+    overflow-wrap: break-word;
+
 
 }
 
